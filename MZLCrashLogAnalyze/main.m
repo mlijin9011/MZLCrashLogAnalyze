0bae7f7fdb13a675137b4c0b90689d8911953648
lijin22 <lijin22@staff.sina.com.cn>
2018-05-01 11:40:19 +0800

    Initial Commit

---
+++ MZLCrashLogAnalyze/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MZLCrashLogAnalyze
+//
+//  Created by lijin22 on 2018/5/1.
+//  Copyright © 2018年 lijin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
